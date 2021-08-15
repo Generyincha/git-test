26342bc450da440ec403f77e2fe0053b9f412b46
Genvy <g20208728@xs.ustb.edu.cn>
2021-08-15 16:24:02 +0800

    first version

---
+++ src/main/java/com/saee/java/GitTest.java
@@ -0,0 +1,11 @@
+package com.saee.java;
+
+/**
+ * @author ustbwjw
+ * @create 2021-08-15 16:22
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("git");
+    }
+}
